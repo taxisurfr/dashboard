bd1d35440eac4cf6dd72f700d295ad27936f2731
peter <peter.hall@star-trac.de>
2017-10-10 21:38:53 +0200

    use old artifacts

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use(express.static('views'));
 app.get('/xxx', function (req, res) {
